32ac03d007f4ef1e83b40a2a9cf6fd921fa39518
Nico Dinata <nicodnt@icloud.com>
2025-03-23 23:48:14 +1100

    set up tailwindcss

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,10 +1,12 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.astro",
       options: { parser: "astro" },
     },
   ],
+  // This needs to point to the site's CSS entrypoint file.
+  tailwindStylesheet: "./src/styles/global.css",
 };
